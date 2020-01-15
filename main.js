513b73c8562f681c097a786f2ac4f99946e5e431
mezzi <mezzija@mail.ru>
2020-01-16 00:46:06 +0300

    lesson 4 correct version

--- main.js
+++ main.js
@@ -6,32 +6,15 @@ let text1=document.getElementById('text1');
 let text2=document.getElementById('text2');
 let text3=document.getElementById('text3');
 const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-let changeStyle=(value)=>{
+const changeStyle=()=>{
     let i=0;
-    if(value==='text1'){
-        return ()=>{
-            text1.style.color=colors[i];
-            i++;
-            if(i>=colors.length) i=0;
-            return i;
-        }
-    }else if(value==='text2'){
-        return ()=>{
-            text2.style.color=colors[i];
-            i++;
-            if(i>=colors.length) i=0;
-            return i;
-        }
-    }else if (value==='text3'){
-        return ()=>{
-            text3.style.color=colors[i];
-            i++;
-            if(i>=colors.length) i=0;
-            return i;
-        }
-
+    return event=>{
+        event.target.style.color=colors[i];
+        i++;
+        if(i===colors.length) i=0;
+        return i;
     }
 };
-text1.addEventListener('click',changeStyle('text1'));
-text2.addEventListener('click',changeStyle('text2'));
-text3.addEventListener('click',changeStyle('text3'));
+text1.addEventListener('click',changeStyle());
+text2.addEventListener('click',changeStyle());
+text3.addEventListener('click',changeStyle());
