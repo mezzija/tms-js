bdf0176883fef3c74814960546b70a086a47af3d
mezzi <mezzija@mail.ru>
2020-01-16 00:39:53 +0300

    lesson 5 homework+bubble sort

--- main.js
+++ main.js
@@ -1,37 +1,58 @@
-const sum=(a)=>(y)=>a+y;
-let result=sum(5)(2);
-console.log(result);
-
 let text1=document.getElementById('text1');
 let text2=document.getElementById('text2');
 let text3=document.getElementById('text3');
-const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-let changeStyle=(value)=>{
-    let i=0;
-    if(value==='text1'){
-        return ()=>{
-            text1.style.color=colors[i];
-            i++;
-            if(i>=colors.length) i=0;
-            return i;
-        }
-    }else if(value==='text2'){
-        return ()=>{
-            text2.style.color=colors[i];
-            i++;
-            if(i>=colors.length) i=0;
-            return i;
-        }
-    }else if (value==='text3'){
-        return ()=>{
-            text3.style.color=colors[i];
-            i++;
-            if(i>=colors.length) i=0;
-            return i;
-        }
 
-    }
+const changeStyle=id=>event=>{
+    event.target.style.color=colors.next(id).value;
 };
-text1.addEventListener('click',changeStyle('text1'));
-text2.addEventListener('click',changeStyle('text2'));
-text3.addEventListener('click',changeStyle('text3'));
+text1.addEventListener('click',changeStyle(Symbol()));
+text2.addEventListener('click',changeStyle(Symbol()));
+text3.addEventListener('click',changeStyle(Symbol()));
+
+const colors = {
+    data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
+    [Symbol.iterator]() {
+        return this;
+    },
+    next(id){
+        if( this[id]===undefined){
+            this[id]=0;
+        }
+        if(this[id]<this.data.length){
+            return {
+                done:false,
+                value: this.data[this[id]++],
+            }
+        }else {
+            this[id]=0;
+            return {
+                dane:true,
+            }
+        }
+    },
+};
+let bubbleSort=(arr)=>{
+    for( let i=0;i<arr.length-1;i++){
+        let flag=0;
+        for( let i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                let a=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=a;
+                flag++;
+            }
+        }
+        if(flag===0) break;
+    }
+    return arr;
+};
+const randomArr=(length)=>{
+    let arr=new Array(length);
+    for (let i=0;i<arr.length;i++){
+        arr[i]=Math.ceil(Math.random()*100);
+    }
+    return arr;
+};
+let arr=randomArr(10);
+console.log(arr);
+console.log(bubbleSort(arr));
